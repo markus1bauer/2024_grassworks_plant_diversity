88b9ad4be1c8ecf6f9cec9572f83613ec9e3be3f
markus1bauer <markus1.bauer@tum.de>
2025-04-08 17:58:01 +0200

    Create _prepare_data.R

---
+++ R/_prepare_data.R
@@ -0,0 +1,58 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# GRASSWORKS Project
+# Taxonomic plant diversity ####
+# Prepare data
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Markus Bauer
+# 2025-04-08
+
+
+
+### Packages ###
+library(renv)
+library(here)
+library(tidyverse)
+
+### Start ###
+rm(list = ls())
+# installr::updateR(
+#   browse_news = FALSE,
+#   install_R = TRUE,
+#   copy_packages = TRUE,
+#   copy_site_files = TRUE,
+#   keep_old_packages = FALSE,
+#   update_packages = FALSE,
+#   start_new_R = FALSE,
+#   quit_R = TRUE
+#   )
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# A Load data #################################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+sites <- read_csv(
+  here("data", "raw", "sites_processed_environment_nms_20240813.csv"),
+  col_names = TRUE, na = c("na", "NA", ""), col_types = cols(
+    .default = "?",
+    eco.id = "f",
+    region = col_factor(levels = c("north", "centre", "south"), ordered = TRUE),
+    site.type = col_factor(
+      levels = c("positive", "restored", "negative"), ordered = TRUE
+    ),
+    freq.mow = "f",
+    fertilized = "f"
+  )
+)
+
+
+rm(list = setdiff(ls(), c("sites")))
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# C Save processed data #######################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
